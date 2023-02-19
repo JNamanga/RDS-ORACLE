02e10ea02033681a26b949bf7968d338bf6f93d5
JNamanga <125778164+JNamanga@users.noreply.github.com>
2023-02-18 21:37:40 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,31 @@
+variable "access_key" {
+    description = "provide access key"
+    type = string
+    default = "AKIAUYMJGZFVAPUYDGCPvalue"
+  
+}
+
+variable "secret_key" {
+    description = "secret key"
+    type = string
+    default = "I0a3ApgzfaNr38SZGQcOCMhhnuXMpGOgo1pQOPw/"
+  
+}
+
+variable "storage" {
+dedescription = "create storage"  
+type = string
+default = "20"
+}
+
+variable "engine_version" {
+dedescription = "engine version"  
+type = string
+default = "5.7"
+}
+
+variable "username" {
+description = "value" "create storage"  
+type = string
+default = "20Jude"
+}
